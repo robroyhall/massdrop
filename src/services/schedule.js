797e3fec4280d144192472267d1c76e433fd2d6a
robroyhall <robroyhall@gmail.com>
2019-02-20 12:50:01 -0800

    Removed setting db connection to null for better recovery

--- src/services/schedule.js
+++ src/services/schedule.js
@@ -12,7 +12,6 @@ class Schedule {
 		return new Promise((resolve, reject) => {
 			this.connection.connect((err) => {
 				if (err) {
-					this.connection = null;
 					reject({
 						"message": "The database could not be reached"
 					});
