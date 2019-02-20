8fc509318c6ea14e09f877499633e57e58c3cdb6
robroyhall <robroyhall@gmail.com>
2019-02-20 12:52:43 -0800

    Dont set db connection to null for better recovery

--- src/workers/fetch.js
+++ src/workers/fetch.js
@@ -13,7 +13,6 @@ class Fetch {
 		return new Promise((resolve, reject) => {
 			this.connection.connect((err) => {
 				if (err) {
-					this.connection = null;
 					reject({
 						"message": "The database could not be reached"
 					});
